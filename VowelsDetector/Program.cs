c5695b5ed7855e368742d7598a42bd3ccddba4f4
elijah amoako <elijahamoako72@gmail.com>
2022-06-24 21:20:36 +0000

    vowels detector

---
+++ VowelsDetector/Program.cs
@@ -0,0 +1,25 @@
+﻿// a simple vowel and consonant game where when you enter an alphabet,
+// it detects whether it is a vowel or a consonant.
+
+Console.Write("Enter an alphabet: ");
+char alp = Convert.ToChar(Console.ReadLine());
+
+
+switch (alp)
+{
+    case 'i':
+    case 'e':
+    case 'o':
+    case 'u':
+    case 'a':
+        Console.WriteLine($"{alp} is a vowel.");
+        break;
+
+    default:
+        Console.WriteLine($"{alp} is a consonant.");
+        break;
+}
+
+
+
+
